046ca943e586d46ad33c367df13b0850ac08dd23
MariaDelgadoEiriz2 <m.delgadoei@alumnos.urjc.es>
2019-05-31 22:51:00 +0200

    Show Elements with @ModelAttribute

--- gestor-tiempos-maria/src/main/java/es/urjc/daw/app/controllers/WebController.java
+++ gestor-tiempos-maria/src/main/java/es/urjc/daw/app/controllers/WebController.java
@@ -1,11 +1,19 @@
 package es.urjc.daw.app.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import es.urjc.daw.app.category.Category;
+import es.urjc.daw.app.category.CategoryService;
+import es.urjc.daw.app.event.Event;
+import es.urjc.daw.app.event.EventService;
+import es.urjc.daw.app.interval.Interval;
+import es.urjc.daw.app.interval.IntervalService;
 import es.urjc.daw.app.user.UserComponent;
 
 
@@ -13,7 +21,11 @@ import es.urjc.daw.app.user.UserComponent;
 public class WebController {
 	
 	@Autowired private UserComponent userComponent;
-	
+	@Autowired private  CategoryService categoryService;	
+	@Autowired private  EventService eventService;	
+	@Autowired private  IntervalService intervalService;	
+
+	 
 	@ModelAttribute
 	public void addUserToModel(Model model) {
 		boolean logged = userComponent.getLoggedUser() != null;
@@ -22,6 +34,15 @@ public class WebController {
 			model.addAttribute("admin", userComponent.getLoggedUser().getRoles().contains("ROLE_ADMIN"));
 			model.addAttribute("username",userComponent.getLoggedUser().getEmail());
 		}
+	}
+	@ModelAttribute
+	public void addElementsToModel(Model model) {
+		List<Category> categories = categoryService.findAll();
+		model.addAttribute("categorias", categories);
+		List<Event> events = eventService.findAll();
+		model.addAttribute("eventos", events);
+		List<Interval> intervals = intervalService.findAll();
+		model.addAttribute("intervalos", intervals);
 	}
 	
 	@GetMapping("/")
