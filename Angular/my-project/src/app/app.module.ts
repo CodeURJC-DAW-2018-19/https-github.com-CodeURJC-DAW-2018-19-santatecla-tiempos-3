ab9295965f61cebbe796c6f3c7ece2c717ded655
MariaDelgadoEiriz2 <m.delgadoei@alumnos.urjc.es>
2019-06-04 22:18:14 +0200

    Add dependencies to ng-project

--- Angular/my-project/src/app/app.module.ts
+++ Angular/my-project/src/app/app.module.ts
@@ -1,34 +1,202 @@
+/**
+ * EXTERNAL COMPONENTS
+ */
+//angular
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { JsonpModule } from '@angular/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { MatIconRegistry } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { ScrollDispatchModule } from '@angular/cdk/scrolling';
+import { A11yModule } from '@angular/cdk/a11y';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { PortalModule } from '@angular/cdk/portal';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { CdkStepperModule } from '@angular/cdk/stepper';
+import { CdkTableModule } from '@angular/cdk/table';
+import { CdkTreeModule } from '@angular/cdk/tree';
+import {
+  MatBadgeModule,
+  MatBottomSheetModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDividerModule,
+  MatExpansionModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatSortModule,
+  MatStepperModule,
+  MatTableModule,
+  MatTreeModule,
+} from '@angular/material';
+import {
+  MatButtonModule,
+  MatListModule,
+  MatIconModule,
+  MatCardModule,
+  MatMenuModule,
+  MatInputModule,
+  MatButtonToggleModule,
+  MatProgressSpinnerModule,
+  MatSelectModule,
+  MatSlideToggleModule,
+  MatDialogModule,
+  MatSnackBarModule,
+  MatToolbarModule,
+  MatTabsModule,
+  MatSidenavModule,
+  MatTooltipModule,
+  MatRippleModule,
+  MatRadioModule,
+  MatGridListModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatSliderModule,
+  MatAutocompleteModule,
+} from '@angular/material';
 
+import { DomSanitizer } from '@angular/platform-browser';
+
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+//covalent
+import {
+  CovalentCommonModule,
+  CovalentLayoutModule,
+  CovalentMediaModule,
+  CovalentExpansionPanelModule,
+  CovalentStepsModule,
+  CovalentLoadingModule,
+  CovalentDialogsModule,
+  CovalentSearchModule,
+  CovalentPagingModule,
+  CovalentNotificationsModule,
+  CovalentMenuModule,
+  CovalentDataTableModule,
+  CovalentMessageModule,
+} from '@covalent/core';
+
+import { CovalentBaseEchartsModule } from '@covalent/echarts/base';
+import { CovalentBarEchartsModule } from '@covalent/echarts/bar';
+import { CovalentTooltipEchartsModule } from '@covalent/echarts/tooltip';
+
+
+
+/**
+ * APPLICATION COMPONENTS
+ */
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CovalentLayoutModule } from '@covalent/core/layout';
-import { CovalentStepsModule  } from '@covalent/core/steps';
-/* any other core modules */
-// (optional) Additional Covalent Modules imports
-import { CovalentHttpModule } from '@covalent/http';
-import { CovalentHighlightModule } from '@covalent/highlight';
-import { CovalentMarkdownModule } from '@covalent/markdown';
-import { CovalentDynamicFormsModule } from '@covalent/dynamic-forms';
-// other imports
+import { routing } from './app.routing';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    CovalentBaseEchartsModule,
+    CovalentBarEchartsModule,
+    CovalentTooltipEchartsModule,
     BrowserModule,
-    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    RouterModule.forRoot([]),
+    HttpClientModule,
+    JsonpModule,
+    ScrollDispatchModule,
+    /** Material Modules */
+    MatButtonModule,
+    MatListModule,
+    MatIconModule,
+    MatCardModule,
+    MatMenuModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonToggleModule,
+    MatSlideToggleModule,
+    MatProgressSpinnerModule,
+    MatDialogModule,
+    MatSnackBarModule,
+    MatToolbarModule,
+    MatTabsModule,
+    MatPaginatorModule,
+    MatExpansionModule,
+    MatSidenavModule,
+    MatTooltipModule,
+    MatRippleModule,
+    MatRadioModule,
+    MatGridListModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSliderModule,
+    MatAutocompleteModule,
+    MatInputModule,
+    /** Covalent Modules */
+    CovalentCommonModule,
     CovalentLayoutModule,
+    CovalentMediaModule,
+    CovalentExpansionPanelModule,
     CovalentStepsModule,
-    // (optional) Additional Covalent Modules imports
-    CovalentHttpModule.forRoot(),
-    CovalentHighlightModule,
-    CovalentMarkdownModule,
-    CovalentDynamicFormsModule,
+    CovalentDialogsModule,
+    CovalentLoadingModule,
+    CovalentSearchModule,
+    CovalentPagingModule,
+    CovalentNotificationsModule,
+    CovalentMenuModule,
+    CovalentDataTableModule,
+    CovalentMessageModule,
+    /** Additional **/
+    routing,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    { provide: LocationStrategy, useClass: HashLocationStrategy }],
+  bootstrap: [AppComponent],
+  exports:[
+    A11yModule,
+    CdkStepperModule,
+    CdkTableModule,
+    CdkTreeModule,
+    DragDropModule,
+    MatAutocompleteModule,
+    MatBadgeModule,
+    MatBottomSheetModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatStepperModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatTreeModule,
+    PortalModule,
+    ScrollingModule,
+  ]
 })
 export class AppModule { }
