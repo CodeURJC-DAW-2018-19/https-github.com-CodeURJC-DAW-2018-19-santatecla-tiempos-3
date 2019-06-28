44012f91f9a8f63c3eba3a7c9b902d3fcaa81a5d
MariaDelgadoEiriz2 <mary.eiriz@gmail.com>
2019-06-28 13:30:24 +0200

    Sidebar Angular

--- Angular/my-project/src/app/app.component.ts
+++ Angular/my-project/src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef, AfterViewInit, ViewChild} from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconRegistry, MatDialog } from '@angular/material';
+import { TdMediaService, tdRotateAnimation } from '@covalent/core';
+import { environment } from '../environments/environment';
+import { TabsComponent } from './pages/tabs/tabs.component';
+import { TabService } from './tabs/tab.service';
+
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'my-app',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    animations: [tdRotateAnimation],
 })
-export class AppComponent {
-  title = 'my-project';
+export class AppComponent implements AfterViewInit {
+    
+    @ViewChild(TabsComponent) tabsComponent;
+    mode: string;
+
+    constructor(
+        public media: TdMediaService,
+        public dialog: MatDialog,
+        private _changeDetectorRef: ChangeDetectorRef,
+        private _iconRegistry: MatIconRegistry,
+        private _domSanitizer: DomSanitizer,
+        public tabService: TabService
+    ) 
+    {
+        this._iconRegistry.addSvgIconInNamespace(
+            'assets',
+            'covalent',
+            this._domSanitizer.bypassSecurityTrustResourceUrl(
+                'https://raw.githubusercontent.com/Teradata/covalent-quickstart/develop/src/assets/icons/covalent.svg',
+            ),
+        );
+
+        if(environment.production){
+            this.mode = "Production";
+        } else {
+            this.mode = "Development";
+        }
+    }
+
+    ngAfterViewInit(): void {
+        // broadcast to all listener observables when loading the page
+        this.media.broadcast();
+        this._changeDetectorRef.detectChanges();
+    }
+
 }
